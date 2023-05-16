7ad07a57b9b6b1b3c949b61f095b13e72968a7c5
Syaifun Nadhif <nadhifeldrafo@gmail.com>
2023-05-16 20:44:03 +0700

    develop

---
+++ routes/index.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const router = express.Router();
+const user = require('../controllers/user');
+
+const middlewares = require('../utils/middlewares');
+
+router.post('/register', user.register);
+router.post('/login', user.login);
+router.get('/whoami', middlewares.auth, user.whoami);
+
+module.exports = router;
